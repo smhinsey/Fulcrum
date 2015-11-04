8e618c87c91c46fd5719cf24e78f37ca53e97123
Shawn Hinsey <smhinsey@gmail.com>
2015-11-04 01:50:38 -0800

    docs

--- seed/SeedComponents/Components/UserAccounts/Domain/Entities/AppUser.cs
+++ seed/SeedComponents/Components/UserAccounts/Domain/Entities/AppUser.cs
@@ -5,6 +5,7 @@ namespace FulcrumSeed.Components.UserAccounts.Domain.Entities
 {
 	public class AppUser : RelationalUserAccount
 	{
+		// TODO: move these properties to a linked UserProfile type
 		[Display(Name = "First Name")]
 		public virtual string FirstName { get; set; }
 		[Display(Name = "Last Name")]
