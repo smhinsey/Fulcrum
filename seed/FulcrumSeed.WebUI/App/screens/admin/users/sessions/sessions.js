a11988dfd0ff0d0340d6a400fdb499dd30b23ec1
Shawn Hinsey <smhinsey@gmail.com>
2015-12-11 19:47:27 -0800

    setting up user session UI

--- seed/FulcrumSeed.WebUI/App/screens/admin/users/sessions/sessions.js
+++ seed/FulcrumSeed.WebUI/App/screens/admin/users/sessions/sessions.js
@@ -1,4 +1,4 @@
-﻿angular.module('fulcrumSeed.screens.admin.userSessions', [])
+﻿angular.module('fulcrumSeed.screens.admin.users.sessions', [])
 		.config(function ($stateProvider) {
 
 			$stateProvider
@@ -9,10 +9,45 @@
 
 		})
 	.controller('userSessionsController', [
-		'$scope', '$state', '$rootScope',
-		function ($scope, $state, $rootScope) {
+		'$scope', '$state', '$rootScope','querySvc',
+		function ($scope, $state, $rootScope, querySvc) {
 
 			$rootScope.title = "User Sessions";
 
+			$scope.gridOptions = {
+				enableSorting: true,
+				enableFiltering: false,
+				enableHorizontalScrollbar: 0,
+				enableColumnMenus: false,
+				columnDefs: [
+					{ name: 'Username', field: 'name' },
+					{ name: 'Logged In', field: 'name' },
+					{ name: 'Last Seen', field: 'name' },
+					{
+						name: '',
+						field: 'roleId',
+						enableSorting: false,
+						enableFiltering: false,
+						enableColumnMenus: false,
+						cellTemplate: "<i class='glyphicon glyphicon-edit' ng-click='grid.appScope.editRole(row.entity)'></i>",
+						maxWidth: 90
+					},
+				],
+			};
+
+			var init = function () {
+				querySvc.run('UserSessionQueries/GetAll')
+					.then(function (response) {
+
+						var data = response.data.results;
+
+						$scope.gridOptions.data = data;
+
+						$scope.users = response.data.results;
+					}, function (error) {
+						// TODO: ??
+					});
+			};
+			init();
 		}
 	]);
---
+++ seed/FulcrumSeed/Components/UserAccounts/Queries/UserSessionQueries.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Fulcrum.Core;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+
+namespace FulcrumSeed.Components.UserAccounts.Queries
+{
+	public class UserSessionQueries : IQuery
+	{
+		private readonly IRefreshTokenStore _tokenStore;
+
+		public UserSessionQueries(IRefreshTokenStore tokenStore)
+		{
+			_tokenStore = tokenStore;
+		}
+
+		public IEnumerable<ITokenMetadata> GetAll()
+		{
+			return _tokenStore.GetAllAsync("").Result;
+		}
+	}
+}
